5956ffc896e5ec868fcf88df164c3fc1a7371f68
Avie Vasantlal <adv5201@psu.edu>
2025-02-14 02:57:54 -0500

    The glorius seg fault has return for its revenge

--- mm.c
+++ mm.c
@@ -105,6 +105,42 @@ static size_t MAX(size_t x, size_t y) {
     return (x > y) ? x : y;
 }
 
+static void *coalesce(void*bp)
+ {
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    //case 1
+    if (prev_alloc && next_alloc){
+        return bp;
+    }
+    
+    //case 2
+    else if(prev_alloc && !next_alloc){ 
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp),PACK(size,0));
+        PUT(FTRP(bp),PACK(size,0));
+    }
+
+    //case 3
+    else if(!prev_alloc && next_alloc){ 
+        size+=GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp),PACK(size,0));
+        PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));
+        bp=PREV_BLKP(bp);
+    }
+ 
+    //case 4
+    else{ 
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)))+ GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));
+        PUT(FTRP(NEXT_BLKP(bp)),PACK(size,0));
+        bp=PREV_BLKP(bp);
+    }
+    return bp;
+}
+
 static void* extend_heap(size_t words)
 {
     char*bp;
@@ -151,41 +187,7 @@ static void place (void *bp, size_t asize){
 }
 
 
-static void *coalesce(void*bp)
- {
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    size_t size = GET_SIZE(HDRP(bp));
 
-    //case 1
-    if (prev_alloc && next_alloc){
-        return bp;
-    }
-    
-    //case 2
-    else if(prev_alloc && !next_alloc){ 
-        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-        PUT(HDRP(bp),PACK(size,0));
-        PUT(FTRP(bp),PACK(size,0));
-    }
-
-    //case 3
-    else if(!prev_alloc && next_alloc){ 
-        size+=GET_SIZE(HDRP(PREV_BLKP(bp)));
-        PUT(FTRP(bp),PACK(size,0));
-        PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));
-        bp=PREV_BLKP(bp);
-    }
- 
-    //case 4
-    else{ 
-        size += GET_SIZE(HDRP(PREV_BLKP(bp)))+ GET_SIZE(FTRP(NEXT_BLKP(bp)));
-        PUT(HDRP(PREV_BLKP(bp)),PACK(size,0));
-        PUT(FTRP(NEXT_BLKP(bp)),PACK(size,0));
-        bp=PREV_BLKP(bp);
-    }
-    return bp;
-    }
 
  
  /*
